2ff23724fbe0c0ac835313cf20c6baee6d77afa7
Victor Irekponor <marquisvictor93@gmail.com>
2021-10-07 23:24:50 +0100

    navbar complete :rocket:

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button, Menu, Typography, Avatar } from 'antd';
 import {
     HomeOutlined,
-    MoneyCollectedOutlined,
+    MoneyCollectOutlined,
     BulbOutlined,
     FundOutlined,
     MenuOutlined,
@@ -19,6 +19,20 @@ const NavBar = () => {
                     <Link to='/'>Cryptoverse</Link>
                 </Typography.Title>
             </div>
+            <Menu theme='dark'>
+                <Menu.Item icon={<HomeOutlined />}>
+                    <Link to='/'>Home</Link>
+                </Menu.Item>
+                <Menu.Item icon={<FundOutlined />}>
+                    <Link to='cryptocurrencies/'>Cryptocurrencies</Link>
+                </Menu.Item>
+                <Menu.Item icon={<MoneyCollectOutlined />}>
+                    <Link to='/exchanges'>Exchanges</Link>
+                </Menu.Item>
+                <Menu.Item icon={<BulbOutlined />}>
+                    <Link to='/news'>News</Link>
+                </Menu.Item>
+            </Menu>
         </div>
     );
 };
