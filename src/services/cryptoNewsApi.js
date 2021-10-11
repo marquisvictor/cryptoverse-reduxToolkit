cd410cb47cdf449b3e4a9f3fac3555aa15b7d2df
Victor Irekponor <marquisvictor93@gmail.com>
2021-10-11 23:06:39 +0100

    build and add create news endpoint

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,27 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeader = {
+    'x-bingapis-sdk': 'true',
+    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+    'x-rapidapi-key': 'b08d2796abmsh1587b262adbfd02p175bf7jsne900c0f7dbc1',
+};
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com/';
+
+const createRequest = url => ({
+    url,
+    headers: cryptoNewsHeader,
+});
+
+export const cryptoNewsApi = createApi({
+    reducerPath: 'cryptoNewsApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: builder => ({
+        getCryptoNews: builder.query({
+            query: ({ newsCategory, count }) =>
+                createRequest(
+                    `/nnews/search?q=${newsCategory}&safeSearch=off&textFormat=Raw&freshess=Day&count=${count}`,
+                ),
+        }),
+    }),
+});
